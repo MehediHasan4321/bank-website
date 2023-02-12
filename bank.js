12470e71a66af2abb9f8db54b169162105651c76
MehediHasan4321 <126mehedi@gmail.com>
2023-02-12 13:48:47 +0600

    update some calculation

--- bank.js
+++ bank.js
@@ -1,12 +1,15 @@
 const currentBalnce = document.getElementById("currnet-blance");
 let totalBlance = 1240
-
+let totalDeposite = 0;
+let totalWidtdrow = 0;
 document.getElementById("deposite-btn").addEventListener("click", function () {
+    
     const depositeValue = document.getElementById("deposite-input-field").value;
     const depositeblance = document.getElementById("deposite-blance");
 
     if (depositeValue > 0) {
-        depositeblance.innerText = `$${depositeValue}`
+        totalDeposite+= parseInt(depositeValue)
+        depositeblance.innerText = `$${totalDeposite}`
         totalBlance += parseInt(depositeValue)
         currentBalnce.innerText = `$${totalBlance}`
         document.getElementById("deposite-input-field").value = ""
@@ -19,7 +22,8 @@ document.getElementById("witdrow-btn").addEventListener("click", function () {
     const witdrowBlance = document.getElementById("widtdrow-blance")
 
     if(widtdrowValue>0 && widtdrowValue < totalBlance){
-        witdrowBlance.innerText = `$${widtdrowValue}`
+        totalWidtdrow += parseInt(widtdrowValue)
+        witdrowBlance.innerText = `$${totalWidtdrow}`
         totalBlance -= parseInt(widtdrowValue)
         currentBalnce.innerText = `$${totalBlance}`
         document.getElementById("witdrow-input-field").value = ""
