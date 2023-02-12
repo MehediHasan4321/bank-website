5511e08450071757616ad26e0096db16af46a180
MehediHasan4321 <126mehedi@gmail.com>
2023-02-12 10:17:31 +0600

    complet index.html section

---
+++ index.js
@@ -0,0 +1,10 @@
+document.getElementById("submit-btn").addEventListener("click",function(e){
+    const email = document.getElementById("email-field").value;
+    const password = document.getElementById("password-field").value
+    if(email === "126mehedi@gmail.com" && password === "1234"){
+        window.location.href = "bank.html"
+    }else{
+        alert("invalid user")
+    }
+    document.getElementById("password-field").value = ""
+})
