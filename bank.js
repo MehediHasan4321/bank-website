40be2a954a80ea973c190631c86f820d40709672
MehediHasan4321 <126mehedi@gmail.com>
2023-02-13 16:47:39 +0600

    fix a bug

--- bank.js
+++ bank.js
@@ -2,50 +2,51 @@
 //All Event Lestener Are Here
 
 document.getElementById("deposite-btn").addEventListener("click", function () {
-  
+
     const depositeValue = inputFieldValueToNumber("deposite-input-field");
-    const deposite = strToNumFromElement("deposite-blance")
-    const current = strToNumFromElement("currnet-blance")
-    const depositeTotoal = depositeValue + deposite;
-    setNewValue("deposite-blance",depositeTotoal)
-    const currentTotoal = depositeTotoal + current;
-   
-    
-    setNewValue("currnet-blance",currentTotoal)
-    console.log(depositeValue)
-    console.log(deposite);
-    console.log(current)
+    if (depositeValue > 0) {
+        const deposite = strToNumFromElement("deposite-blance")
+        const current = strToNumFromElement("currnet-blance")
+        const depositeTotoal = depositeValue + deposite;
+        const currentTotoal = depositeTotoal + current;
+
+        setNewValue("deposite-blance", depositeTotoal)
+        setNewValue("currnet-blance", currentTotoal)
+    }
+
 })
 document.getElementById("witdrow-btn").addEventListener("click", function () {
     const widtdrowValue = inputFieldValueToNumber("witdrow-input-field")
-    const witdrow = strToNumFromElement("widtdrow-blance")
-    const current = strToNumFromElement("currnet-blance")
-    const widtdrowTotoal = widtdrowValue + witdrow;
-    const curentTotal = current - widtdrowTotoal
-    setNewValue("widtdrow-blance",widtdrowTotoal)
-    setNewValue("currnet-blance",curentTotal)
+    if (widtdrowValue > 0) {
+        const witdrow = strToNumFromElement("widtdrow-blance")
+        const current = strToNumFromElement("currnet-blance")
+        const widtdrowTotoal = widtdrowValue + witdrow;
+        const curentTotal = current - widtdrowTotoal
+        setNewValue("widtdrow-blance", widtdrowTotoal)
+        setNewValue("currnet-blance", curentTotal)
+    }
 
 
 })
 //All Root Functions Are Here
 
-function inputFieldValueToNumber(inputId){
+function inputFieldValueToNumber(inputId) {
     let inputFieldValue = document.getElementById(inputId).value;
 
     const strToNum = parseFloat(inputFieldValue);
-    document.getElementById(inputId).value = ""   
-    
+    document.getElementById(inputId).value = ""
+
     return strToNum;
 }
 
-function strToNumFromElement(elementId){
+function strToNumFromElement(elementId) {
     const elementText = document.getElementById(elementId).innerText;
     const removeFirstElement = elementText.substring(1);
     const stringToNumber = parseFloat(removeFirstElement);
-    return stringToNumber;  
+    return stringToNumber;
 }
 
-function setNewValue (elementId,newValue){
+function setNewValue(elementId, newValue) {
     const elementName = document.getElementById(elementId);
     elementName.innerText = `$${newValue}`
 }
